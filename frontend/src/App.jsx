1b684f0d8104928d15f2faca78826c4a9f0c5740
domicilionewera-byte <domiciliosnewera@gmail.com>
2026-06-01 00:07:56 -0500

    feat: add Vercel Analytics

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Hero from './components/Hero'
 import InscriptionForm from './components/InscriptionForm'
 import Pricing from './components/Pricing'
@@ -25,6 +26,7 @@ function App() {
         <Route path="/gracias" element={<ThankYou />} />
         <Route path="/admin"   element={<Admin />} />
       </Routes>
+      <Analytics />
     </BrowserRouter>
   )
 }
